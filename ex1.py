32ec125cae7dafae96e25cd1b5fd80894a06d83c
Ashutosh Pathak <iamashutoshpathak@gmail.com>
2017-07-01 16:06:34 +0530

    Initial Commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# This is my first python program.
+print "Hello World!!"
+print "Hello Again"
+print "I like typing this."
+print 'Yay! Printing.'
+print "This is fun."
+print "I'd much rather you'not'."
+print 'I "said" do not touch this.'
---
+++ ex13a.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, alpha, beta, gamma, theta = argv
+
+print "The script is called: ", script
+alpha = raw_input("What is the value of alpha? ")
+beta =  raw_input("What is the value of beta? ")
+gamma = raw_input("What is the value of gamma? ")
+theta = raw_input("What is the value of theta? ")
+
+print alpha, beta, gamma, theta
