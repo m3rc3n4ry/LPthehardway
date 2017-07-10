39f8995d50bb9c9f702ebdd84ee1e6539d7c0ead
Ashutosh Pathak <iamashutoshpathak@gmail.com>
2017-07-10 23:57:59 +0530

    Added exercise 35

--- exercises/ex35.py
+++ exercises/ex35.py
@@ -29,3 +29,6 @@ def bear_room():
         if next == "take honey.":
             dead("The bear looks at you and slaps your face off.")
         elif next == "taunt bear" and not bear_moved:
+            print "The bear has moved from the door. You can go through it now."
+            bear_moved = True
+        elif next == "taunt bear" and 
